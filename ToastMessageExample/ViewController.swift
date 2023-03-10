6be85fded3312253b18adf935329599dcfd91558
SeungUk <krdut1@gmail.com>
2023-03-10 09:20:15 +0900

    Initial Commit

---
+++ ToastMessageExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToastMessageExample
+//
+//  Created by plsystems on 2023/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
